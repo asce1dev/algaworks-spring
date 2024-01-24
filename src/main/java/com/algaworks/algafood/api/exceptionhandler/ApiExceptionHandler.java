c1529f552f01b197aaef1f37e122aa86168ae50d
AEFEEFT <AEFEEFT@PC>
2024-01-24 10:53:56 -0300

    Commit 3

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -47,10 +48,14 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	    BindingResult bindingResult = ex.getBindingResult();
 	    
 	    List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream()
-	    		.map(fieldError -> Problem.Field.builder()
+	    		.map(fieldError -> {
+	    			String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+	    			
+	    		return	Problem.Field.builder()
 	    				.name(fieldError.getField())
-	    				.userMessage(fieldError.getDefaultMessage())
-	    				.build())
+	    				.userMessage(message)
+	    				.build();
+	    		})
 	    		.collect(Collectors.toList());
 	    
 	    Problem problem = createProblemBuilder(status, problemType, detail)
